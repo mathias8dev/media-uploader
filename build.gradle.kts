19be9814f5b5a5e22887016f2bd0447a6cfe8e34
mathias8dev <kalipemathias.pro@gmail.com>
2025-01-13 12:13:01 +0100

    feature: setup of dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,33 @@ repositories {
     mavenCentral()
 }
 
+val coroutinesVersion by extra("1.9.0")
+
 dependencies {
+
+    // https://mvnrepository.com/artifact/com.amazonaws/aws-java-sdk-s3
+    implementation("com.amazonaws:aws-java-sdk-s3:1.12.740")
+
+    implementation(platform("org.apache.tika:tika-bom:2.9.1"))
+    implementation("org.apache.tika:tika-core")
+
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.7.0")
+
+    //implementation("io.r2dbc:r2dbc-postgresql:0.8.13.RELEASE")
+    implementation("io.r2dbc:r2dbc-pool:1.0.2.RELEASE")
+    implementation("io.r2dbc:r2dbc-spi:1.0.0.RELEASE")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.springframework:spring-jdbc")
+    implementation("org.liquibase:liquibase-core")
+    runtimeOnly("org.postgresql:r2dbc-postgresql:1.0.7.RELEASE")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
